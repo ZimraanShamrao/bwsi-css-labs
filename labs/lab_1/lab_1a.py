badfe7d9e1e3ab4516f76025075155c65d15f3ca
Zimraan Shamrao <84818314+ZimraanShamrao@users.noreply.github.com>
2026-02-15 13:26:29 -0800

    Introduction line created

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Zimraan Shamrao" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print('Hi everyone! My name is Zimraan Shamrao and I am a junior from California. An interesting fact about me is that I compete as a competitive archer.')
 
 if __name__ == "__main__":
     main()
