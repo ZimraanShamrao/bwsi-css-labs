0b0737c5cdb4daf4aa44ff79284500fd99654fdd
Zimraan Shamrao <84818314+ZimraanShamrao@users.noreply.github.com>
2026-02-15 13:40:29 -0800

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -3,13 +3,15 @@ lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+
+robot_speed = 5 # m/s
+
 """
 
 def main():
     print("Hello World!")
 
     name = "Zimraan Shamrao" # TODO: Insert your name between the double quotes
-
     print(f"{name}, Welcome to the CSS course!")
     print('Hi everyone! My name is Zimraan Shamrao and I am a junior from California. An interesting fact about me is that I compete as a competitive archer.')
 
